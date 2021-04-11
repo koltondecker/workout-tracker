58c0f30db7afa89b048cfd323c94d57dbac1282b
Kolton Decker <deckerman25@gmail.com>
2021-04-11 16:35:12 -0500

    Fixed range route for returning last 7 workouts to stats page.

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,6 @@
 let mongoose = require("mongoose");
 let db = require("../models");
+const date = new Date();
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
@@ -8,7 +9,7 @@ mongoose.connect("mongodb://localhost/workout", {
 
 let workoutSeed = [
   {
-    day: new Date().setDate(new Date().getDate()-10),
+    day: date.setDate(date.getDate()-10),
     exercises: [
       {
         type: "resistance",
@@ -21,7 +22,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-9),
+    day: date.setDate(date.getDate()-9),
     exercises: [
       {
         type: "resistance",
@@ -34,7 +35,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-8),
+    day: date.setDate(date.getDate()-8),
     exercises: [
       {
         type: "resistance",
@@ -47,7 +48,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-7),
+    day: date.setDate(date.getDate()-7),
     exercises: [
       {
         type: "cardio",
@@ -58,7 +59,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-6),
+    day: date.setDate(date.getDate()-6),
     exercises: [
       {
         type: "resistance",
@@ -71,7 +72,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate()-5),
+    day: date.setDate(date.getDate()-5),
     exercises: [
       {
         type: "resistance",
@@ -84,7 +85,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date(new Date().setDate(new Date().getDate() - 4)),
+    day: date.setDate(date.getDate() - 4),
     exercises: [
       {
         type: "resistance",
@@ -97,7 +98,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date(new Date().setDate(new Date().getDate() - 3)),
+    day: date.setDate(date.getDate() - 3),
     exercises: [
       {
         type: "resistance",
@@ -110,7 +111,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date(new Date().setDate(new Date().getDate() - 2)),
+    day: date.setDate(date.getDate() - 2),
     exercises: [
       {
         type: "resistance",
--- server.js
+++ server.js
@@ -15,7 +15,23 @@ app.use(logger("dev"));
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.resolve("./public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.resolve("./public/stats.html"));
+});
 
 app.get("/api/workouts", async (req, res) => {
   try {
@@ -36,14 +52,6 @@ app.get("/api/workouts", async (req, res) => {
   }
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve("./public/index.html"));
-});
-
-app.get("/exercise", (req, res) => {
-  res.sendFile(path.resolve("./public/exercise.html"));
-});
-
 app.get("/exercise/:id", async (req, res) => {
   try {
     const workouts = await db.Workout.find({ _id: req.params.id });
@@ -53,15 +61,37 @@ app.get("/exercise/:id", async (req, res) => {
   }
 });
 
+app.get("/api/workouts/range", async (req, res) => {
+  try {
+    const workouts = await db.Workout.aggregate([
+      {
+        $sort: {
+          day: -1
+        }
+      },
+      {
+        $limit: 7
+      },
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration"
+          }
+        }
+      }
+    ]);
+    console.log(workouts);
+    workouts.reverse();
+    res.json(workouts);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
 app.post("/api/workouts", async (req, res) => {
   try {
-    const workout = await db.Workout.create(
-      {
-        day: new Date().setDate(new Date().getDate()),
-        exercises: req.body
-      }
-    );
-    res.json(workouts);
+    const workout = await db.Workout.create({});
+    res.json(workout);
   } catch (error) {
     res.json(error)
   }
@@ -71,28 +101,16 @@ app.put("/api/workouts/:id", async (req, res) => {
   const workoutId = req.params.id;
   
   try {
-    if(!workoutId) {
-      const newWorkout = await db.Workout.create({});
-
-      const workouts = await newWorkout.updateOne({
+    const workouts = await db.Workout.updateOne(
+      {
+        _id: req.params.id
+      },
+      {
         $push: {
           "exercises": req.body
         }
-      });
-
-      newWorkout.save();
-    } else {
-      const workouts = await db.Workout.updateOne(
-        {
-          _id: req.params.id
-        },
-        {
-          $push: {
-            "exercises": req.body
-          }
-        }
-      );
-    }
+      }
+    );
 
     res.json(workouts);
   } catch (error) {
